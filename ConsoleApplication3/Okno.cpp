4fbdd255ee847b20a3ccabbe39634499bde50627
Gracjan Jasiówka <yushimeno@gmail.com>
2017-11-13 14:41:38 +0100

    cos

--- ConsoleApplication3/Okno.cpp
+++ ConsoleApplication3/Okno.cpp
@@ -1,7 +1,36 @@
-#include "okno.h"
+#include "Okno.h"
 
-int _tmain(int argc, _TCHAR* argv[])
+int main(int argc, _TCHAR* argv[])
 {
+	Okno okno(786, 1024, 100, 100);
+	okno.stworzenieOkna(argc, argv);
+	glutDisplayFunc(okno.wyswietl);
+
+	glClearColor(1.0f, 0.8f, 0.9f, 0.0f);
+
+	glutMainLoop();
 
 	return 0;
+}
+
+Okno::Okno(int wysokoscOkna, int szerokoscOkna, int polozenieOknaX, int polozenieOknaY)
+{
+	this->wysokoscOkna = wysokoscOkna;
+	this->szerokoscOkna = szerokoscOkna;
+	this->polozenieOknaX = polozenieOknaX;
+	this->polozenieOknaY = polozenieOknaY;
+}
+
+Okno::Okno()
+{
+		 wysokoscOkna = 768;
+		 szerokoscOkna = 1024;
+		 polozenieOknaX = 100;
+		 polozenieOknaY = 100;
+}
+
+void Okno::stworzenieOkna(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 }
