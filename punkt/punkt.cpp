21bb13adb575676a68747b159a0d6a68106211dc
Gracjan <murbanczyk1996@outlook.com>
2017-11-27 14:38:56 +0100

    Wreszcie porządek

--- punkt/punkt.cpp
+++ punkt/punkt.cpp
@@ -3,8 +3,10 @@
 int main(int argc, CHAR* argv[])
 {
 	punkt okno(786, 1024, 100, 100);
-	glutDisplayFunc(okno.wyswietl);
 
+	okno.stworzenieVAO();
+	okno.stworzenieVBO();
+	glutDisplayFunc(okno.wyswietl);
 	glClearColor(1.0f, 0.8f, 0.9f, 0.0f);
 
 	glutMainLoop();
@@ -34,6 +36,13 @@ punkt::punkt(void)
 void punkt::wyswietl()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glVertexAttribLPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glDrawArrays(GL_POINTS, 0, 1);
+
+	glDisableVertexAttribArray(0);
+	
 	glutSwapBuffers();
 }
 
@@ -61,4 +70,13 @@ void punkt::stworzenieVAO()
 {
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
+}
+
+void punkt::stworzenieVBO()
+{
+	GLfloat Wierzcholki[3] = { 0.0f, 0.0f, 0.0f };
+
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Wierzcholki), Wierzcholki, GL_STATIC_DRAW);
 }
